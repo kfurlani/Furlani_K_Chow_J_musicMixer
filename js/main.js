d1fd15a82f8669f785174c62365080e884c73896
Joanna C <joannachowhy@gmail.com>
2022-04-03 23:25:34 -0600

    revise design and add lottie animation

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
     let puzzlePieces = document.querySelectorAll('.puzzle-pieces *'),
         dropZones = document.querySelectorAll('.drop-zone'),
         theDropBoard = document.querySelector('.puzzle-board');
+    // lottie animation
+    const bird = document.querySelector(".bird-anim");
 
     function startDrag (event) {
         console.log('start dragging');
@@ -14,21 +16,37 @@
 		    console.log('dragged over board');
     }
 
-	function handleDrop (event) {
-		event.preventDefault ();
-		console.log('dropped on board');
-		let currentEl =
-      event.dataTransfer.getData('draggedElement');
+  	function handleDrop (event) {
+  		event.preventDefault ();
+  		console.log('dropped on board');
+  		let currentEl =
+        event.dataTransfer.getData('draggedElement');
 
-		console.log(`dropped this element:`, currentEl);
-		this.appendChild(document.querySelector(`#${currentEl}`));
+  		console.log(`dropped this element:`, currentEl);
+  		this.appendChild(document.querySelector(`#${currentEl}`));
 
-    let audioIndex = document.querySelector(`#${currentEl}`).dataset.drop;
-    let audioClip = document.querySelector((`audio[data-drop="${audioIndex}"]`));
+      let audioIndex = document.querySelector(`#${currentEl}`).dataset.drop;
+      let audioClip = document.querySelector((`audio[data-drop="${audioIndex}"]`));
 
-    audioClip.play();
-    audioClip.loop = true;
-	}
+      audioClip.play();
+      audioClip.loop = true;
+
+      if (currentEl == "topLeft") {
+        // play lottie animation
+        console.log(currentEl);
+        let birdAnim = bodymovin.loadAnimation({
+          wrapper: bird,
+          renderer: "svg",
+          loop: true,
+          autoplay: true,
+          path: "data/birdsFly.json"
+        })
+
+        function playAnimation() {
+          birdAnim.play();
+        }
+      }
+  	}
 
     puzzlePieces.forEach(piece => piece.addEventListener('dragstart', startDrag));
     dropZones.forEach(zone => {
