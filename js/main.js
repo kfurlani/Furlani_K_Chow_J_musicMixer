a9fc048ebf0ffa5caf1bc54ce97f599af787a2d2
Krystal Furlani <krystal@Krystals-MacBook-Pro.local>
2022-04-06 01:46:54 -0400

    trying to add pause,play,reset...not working

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 (() => {
     let puzzlePieces = document.querySelectorAll('.puzzle-pieces *'),
         dropZones = document.querySelectorAll('.drop-zone'),
-        theDropBoard = document.querySelector('.puzzle-board');
+        theDropBoard = document.querySelector('.puzzle-board'),
+        resetPieces = document.querySelectorAll('resetButton'),
+        theSound = document.querySelector('audio'),
+        playButton = document.getElementById('playButton'),
+        pauseButton = document.getElementById('pauseButton');
     // lottie animation
     const bird = document.querySelector(".bird-anim");
 
@@ -26,11 +30,22 @@
   		console.log(`dropped this element:`, currentEl);
   		this.appendChild(document.querySelector(`#${currentEl}`));
 
+      function resetImgs (){
+        dropZones.forEach(zone => {
+           if (zone.childElementCount > 0) {
+                resetPieces.appendChild(zone.firstElementChild);
+            }
+        })
+    }
+
       let audioIndex = document.querySelector(`#${currentEl}`).dataset.drop;
       let audioClip = document.querySelector((`audio[data-drop="${audioIndex}"]`));
 
       audioClip.play();
       audioClip.loop = true;
+
+      function playSound(event) {theSound.play();}
+      function pauseSound(event){theSound.pause();}
 
       if (currentEl == "topLeft") {
         // play lottie animation
@@ -48,7 +63,9 @@
         }
       }
   	}
-
+    //resetPieces.addEventListener('click, resetImgs');
+    //playButton.addEventListener('click, playSound');
+    //pauseButton.addEventListener('click, pauseSound');
     puzzlePieces.forEach(piece => piece.addEventListener('dragstart', startDrag));
     dropZones.forEach(zone => {
         zone.addEventListener ('dragover' , draggedOver);
